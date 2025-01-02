71617ebb18c122d4448d347e3d2ca86ba7748315
williams-nathan <86615333+williams-nathan@users.noreply.github.com>
2025-01-02 14:22:42 -0500

    Update Chapter1_Visualization.R

--- Chapter1_Visualization.R
+++ Chapter1_Visualization.R
@@ -4,3 +4,25 @@ pacman::p_load(
   ggthemes
 )
 ggthemes_data
+
+#First steps
+penguins
+
+glimpse(penguins)
+
+?penguins
+
+## Creatnig a ggplot
+
+ggplot(data = penguins)
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+)
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point()
